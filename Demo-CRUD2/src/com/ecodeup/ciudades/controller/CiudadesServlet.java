c81b172da4fe643638674dfef8cd5c7c29341d22
Diego Alexander Murillo  Suárez <89222624+DiegoMurillo7536@users.noreply.github.com>
2022-05-21 21:03:53 -0500

    Servlet listo
    Ánimo Dams, tú puedes bro!!!

---
+++ Demo-CRUD2/src/com/ecodeup/ciudades/controller/CiudadesServlet.java
@@ -0,0 +1,146 @@
+package com.ecodeup.ciudades.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ecodeup.ciudades.dao.CiudadDAO;
+import com.ecodeup.ciudades.model.Ciudad;
+
+/**
+ * Servlet implementation class AdminArticulo
+ */
+@WebServlet("/CiudadesServlet")
+public class CiudadesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	CiudadDAO ciudadDAO;
+
+	public void init() {
+		String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+		String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+		String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+		try {
+
+			ciudadDAO = new CiudadDAO(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CiudadesServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Hola Servlet..");
+		String action = request.getParameter("action");
+		System.out.println(action);
+		try {
+			switch (action) {
+			case "index":
+				index(request, response);
+				break;
+			case "nuevo":
+				nuevo(request, response);
+				break;
+			case "register":
+				System.out.println("entro");
+				registrar(request, response);
+				break;
+			case "mostrar":
+				mostrar(request, response);
+				break;
+			case "showedit":
+				showEditar(request, response);
+				break;	
+			case "editar":
+				editar(request, response);
+				break;
+			case "eliminar":
+				eliminar(request, response);
+				break;
+			default:
+				break;
+			}			
+		} catch (SQLException e) {
+			e.getStackTrace();
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Hola Servlet..");
+		doGet(request, response);
+	}
+	
+	private void index (HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
+		//mostrar(request, response);
+		RequestDispatcher dispatcher= request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void registrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		Ciudad ciudad = new Ciudad(Integer.parseInt(request.getParameter("id_ciudad")),Integer.parseInt(request.getParameter("id_documento_fk")), request.getParameter("nom_ciudad"));
+		ciudadDAO.insertar(ciudad);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	private void nuevo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/register.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+	private void mostrar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException , ServletException{
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/mostrar.jsp");
+		List<Ciudad> listaCiudades= ciudadDAO.listarCiudades();
+		request.setAttribute("lista", listaCiudades);
+		dispatcher.forward(request, response);
+	}	
+	
+	private void showEditar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		Ciudad ciudad = ciudadDAO.obtenerPorId(Integer.parseInt(request.getParameter("id_ciudad")));
+		request.setAttribute("ciudad", ciudad);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/vista/editar.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	private void editar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
+		Ciudad ciudad= new Ciudad(Integer.parseInt(request.getParameter("id_nombre")),Integer.parseInt(request.getParameter("id_departamento_fk")), request.getParameter("nom_ciudad"));
+		ciudadDAO.actualizar(ciudad);
+		index(request, response);
+	}
+	
+	private void eliminar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
+		Ciudad ciudad = ciudadDAO.obtenerPorId(Integer.parseInt(request.getParameter("id_ciudad")));
+		ciudadDAO.eliminar(ciudad);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+}
